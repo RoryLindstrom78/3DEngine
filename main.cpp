761448dcd681d7f438bbf1d567431325d0a32571
Rory Lindstrom <rorylindstrom78@gmail.com>
2025-08-04 11:29:57 -0700

    Added two header files I used in my original project

--- main.cpp
+++ main.cpp
@@ -1,4 +1,12 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <iostream>
+#include "camera.h"
 
 int main() {
 	std::cout << "Hello World!" << std::endl;
